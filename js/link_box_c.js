69389a81862d932c6bebc57d70263116a2884dd4
manamimurakami <manaphy35@yahoo.co.jp>
2016-11-03 22:30:25 +0000

    20161104 0730

---
+++ js/link_box_c.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("#link_btn_c").click(function(){
+    $("#link_list_c").slideToggle();
+  });
+});
---
+++ js/link_box_p.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("#link_btn_p").click(function(){
+    $("#link_list_p").slideToggle();
+  });
+});
